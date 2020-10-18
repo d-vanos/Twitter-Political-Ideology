047311982195ed3a863f79d91d1ca938c73527ef
d-vanos <danielle.vanos@unimelb.edu.au>
2020-10-19 09:19:37 +1000

    latest working version before upload

--- scripts/rate-twitter-profiles/server.R
+++ scripts/rate-twitter-profiles/server.R
@@ -14,59 +14,195 @@ shinyServer(function(input, output) {
     
     drive_auth()
     sheets_auth()
-
+    
+    # FUNCTIONS
+    # Reduce datasets down
+    reduce <- function(data){
+        data <- data %>%
+            select(ID, Name, Twitter_Bio, Twitter_Handle, selected) %>%
+            mutate(ID = as.numeric(ID),
+                   selected = as.numeric(selected),
+                   selected = ifelse(selected == -99, NA, selected))
+        return(data)
+    }
+    
+    # Round
+    round_down <- function(num){floor(num/10)*10}
+    
+    # Set reactive values
+    data <- reactiveValues()
+    
+    # EVO
     # Import data
     evo_ID <- gs4_find("hbes_3_extract_user")$id
     evo_extracted <- read_sheet(evo_ID, sheet = 1)
-    evo_extracted <- evo_extracted %>%
-        select(ID, Name, Twitter_Bio, Twitter_Handle, selected) %>%
-        mutate(ID = as.numeric(ID),
-               selected = as.numeric(selected),
-               selected = ifelse(selected == -99, NA, selected))
-    data <- reactiveValues()
-    data$evo_extracted <- evo_extracted
-
-
+    
+    # Format
+    evo_extracted <- reduce(evo_extracted)
+    
     # Initialising counter
     evo_counter <- reactiveValues(countervalue = evo_extracted$'ID'[which(is.na(evo_extracted$selected))[1]])
-    evo_counter_upload <- reactiveValues(countervalue = 10-evo_extracted$'ID'[which(is.na(evo_extracted$selected))[1]])
-
+    evo_NAs <- ifelse(is.na(evo_extracted$'ID'[which(is.na(evo_extracted$selected))[1]]), 0, evo_extracted$'ID'[which(is.na(evo_extracted$selected))[1]])
+    evo_counter_upload <- reactiveValues(countervalue = 10-evo_NAs)
+    
+    data$evo_extracted <- evo_extracted
+    
     # Creating tables
     output$evo_table = DT::renderDataTable({data$evo_extracted %>%
-                                                filter(ID == evo_counter$countervalue) %>%
-                                                select(-ID)},
+            filter(ID == evo_counter$countervalue) %>%
+            select(-ID)},
             selection = 'single',
             options = list(dom = 't', # Only shows table (no search, number of entries, etc.)
                            ordering=F)) # Stops sorting columns
+    
+    # SOCIAL
+    social_ID <- gs4_find("spsp_3_extract_user")$id
+    social_extracted <- read_sheet(social_ID, sheet = 1)
+    
+    # Format
+    social_extracted <- reduce(social_extracted)
+
+    # Initialising counter
+    social_counter <- reactiveValues(countervalue = social_extracted$'ID'[which(is.na(social_extracted$selected))[1]])
+    social_NAs <- ifelse(is.na(social_extracted$'ID'[which(is.na(social_extracted$selected))[1]]), 0, social_extracted$'ID'[which(is.na(social_extracted$selected))[1]])
+    social_counter_upload <- reactiveValues(countervalue = 10-social_NAs)
+    
+    data$social_extracted <- social_extracted
+    
+    # Creating table
+    output$social_table = DT::renderDataTable({data$social_extracted %>%
+            filter(ID == social_counter$countervalue) %>%
+            select(-ID)},
+            selection = 'single',
+            options = list(dom = 't', 
+                           ordering=F)) 
+    
+    # BIO
+    bio_ID <- gs4_find("evoldir_3_extract_user")$id
+    bio_extracted <- read_sheet(bio_ID, sheet = 1)
+
+    # Format
+    bio_extracted <- reduce(bio_extracted)
+
+    # Initialising counter
+    bio_counter <- reactiveValues(countervalue = bio_extracted$'ID'[which(is.na(bio_extracted$selected))[1]])
+    bio_NAs <- ifelse(is.na(bio_extracted$'ID'[which(is.na(bio_extracted$selected))[1]]), 0, bio_extracted$'ID'[which(is.na(bio_extracted$selected))[1]])
+    bio_counter_upload <- reactiveValues(countervalue = 10-bio_NAs)
+
+    data$bio_extracted <- bio_extracted
+    
+    # Creating table
+    output$bio_table = DT::renderDataTable({data$bio_extracted %>%
+            filter(ID == bio_counter$countervalue) %>%
+            select(-ID)},
+            selection = 'single',
+            options = list(dom = 't', 
+                           ordering=F)) 
 
     # Number of entries before upload
-    output$count <- renderText({
+    output$evo_count <- renderText({
         paste("Number of entries before upload:", evo_counter_upload$countervalue)   # print the latest value stored in the reactiveValues object
     })
+    
+    output$evo_total <- renderText({
+        paste("Total left:", (max(data$evo_extracted$'ID') - evo_counter$countervalue))  
+    })
+    
+    output$social_count <- renderText({
+        paste("Number of entries before upload:", social_counter_upload$countervalue) 
+    })
+    
+    output$social_total <- renderText({
+        paste("Total left:", (max(data$social_extracted$'ID') - social_counter$countervalue))  
+    })
+    
+    output$bio_count <- renderText({
+        paste("Number of entries before upload:", bio_counter_upload$countervalue)   
+    })
+    
+    output$bio_total <- renderText({
+        paste("Total left:", (max(data$bio_extracted$'ID') - bio_counter$countervalue))  
+    })
 
-    #Click Next and Back
-    observeEvent(input$increase, {
+    #Click Next
+    observeEvent(input$evo_increase, {
         row_ID <- which(data$evo_extracted$'ID' == evo_counter$countervalue)
         data$evo_extracted$'selected'[row_ID] <- 0
         data$evo_extracted$'selected'[row_ID[1] + input$evo_table_rows_selected - 1] <- 1 # Of the people with a particular ID, selecting the correct row
         evo_counter$countervalue = evo_counter$countervalue + 1
         evo_counter_upload$countervalue = evo_counter_upload$countervalue - 1
     })
-
-    observeEvent(input$decrease, {
-        evo_counter$countervalue = evo_counter$countervalue - 1
+    
+    observeEvent(input$social_increase, {
+        row_ID <- which(data$social_extracted$'ID' == social_counter$countervalue)
+        data$social_extracted$'selected'[row_ID] <- 0
+        data$social_extracted$'selected'[row_ID[1] + input$social_table_rows_selected - 1] <- 1 # Of the people with a particular ID, selecting the correct row
+        social_counter$countervalue = social_counter$countervalue + 1
+        social_counter_upload$countervalue = social_counter_upload$countervalue - 1
+    })
+    
+    observeEvent(input$bio_increase, {
+        row_ID <- which(data$bio_extracted$'ID' == bio_counter$countervalue)
+        data$bio_extracted$'selected'[row_ID] <- 0
+        data$bio_extracted$'selected'[row_ID[1] + input$bio_table_rows_selected - 1] <- 1 # Of the people with a particular ID, selecting the correct row
+        bio_counter$countervalue = bio_counter$countervalue + 1
+        bio_counter_upload$countervalue = bio_counter_upload$countervalue - 1
     })
 
-    observeEvent(input$upload, {
-        current_row <- which(data$evo_extracted$'ID' == evo_counter$countervalue)[length(which(data$evo_extracted$'ID' == evo_counter$countervalue))]
-        save_data <- as_tibble(data$evo_extracted$'selected'[1:current_row])
-        colnames(save_data) <- "selected"
-        range <- paste0("R2:R", current_row+1)
+    # Click Back
+    observeEvent(input$evo_decrease, {
+        evo_counter$countervalue = evo_counter$countervalue - 1
+    })
+    
+    observeEvent(input$social_decrease, {
+        social_counter$countervalue = social_counter$countervalue - 1
+    })
+    
+    observeEvent(input$bio_decrease, {
+        bio_counter$countervalue = bio_counter$countervalue - 1
+    })
+
+    # Upload
+    observeEvent(input$evo_upload, {
+        evo_current_row <- which(data$evo_extracted$'ID' == evo_counter$countervalue)[length(which(data$evo_extracted$'ID' == evo_counter$countervalue))]
+        evo_last_saved_row <- which(data$evo_extracted$'ID' == round_down(evo_counter$countervalue))[1]
+        evo_save_data <- as_tibble(data$evo_extracted$'selected'[evo_last_saved_row:evo_current_row])
+        colnames(evo_save_data) <- "selected"
+        evo_save_data <- evo_save_data %>% mutate(selected = ifelse(is.na(selected), -99, selected))
+        evo_range <- paste0("R", evo_last_saved_row+1, ":R", evo_current_row+1)
 
         range_write(ss = evo_ID,
-                    data = save_data,
-                    range = range)
-
+                    data = evo_save_data,
+                    range = evo_range,
+                    col_names = FALSE)
+    })
+    
+    observeEvent(input$social_upload, {
+        social_current_row <- which(data$social_extracted$'ID' == social_counter$countervalue)[length(which(data$social_extracted$'ID' == social_counter$countervalue))]
+        social_last_saved_row <- which(data$social_extracted$'ID' == round_down(social_counter$countervalue))[1]
+        social_save_data <- as_tibble(data$social_extracted$'selected'[social_last_saved_row:social_current_row])
+        colnames(social_save_data) <- "selected"
+        social_save_data <- social_save_data %>% mutate(selected = ifelse(is.na(selected), -99, selected))
+        social_range <- paste0("Q", social_last_saved_row+1, ":Q", social_current_row+1)
+        
+        range_write(ss = social_ID,
+                    data = social_save_data,
+                    range = social_range,
+                    col_names = FALSE)
+    })
+    
+    observeEvent(input$bio_upload, {
+        bio_current_row <- which(data$bio_extracted$'ID' == bio_counter$countervalue)[length(which(data$bio_extracted$'ID' == bio_counter$countervalue))]
+        bio_last_saved_row <- which(data$bio_extracted$'ID' == round_down(bio_counter$countervalue))[1]
+        bio_save_data <- as_tibble(data$bio_extracted$'selected'[bio_last_saved_row:bio_current_row])
+        colnames(bio_save_data) <- "selected"
+        bio_save_data <- bio_save_data %>% mutate(selected = ifelse(is.na(selected), -99, selected))
+        bio_range <- paste0("Q", bio_last_saved_row+1, ":Q", bio_current_row+1)
+        
+        range_write(ss = bio_ID,
+                    data = bio_save_data,
+                    range = bio_range,
+                    col_names = FALSE)
     })
 
     # Saving the data every 10 people
@@ -76,23 +212,72 @@ shinyServer(function(input, output) {
             start_row <- which(data$evo_extracted$'ID' == evo_counter$countervalue - 10)[1] # The first row of the countervalue 5 people ago
             current_row <- which(data$evo_extracted$'ID' == evo_counter$countervalue-1)[length(which(data$evo_extracted$'ID' == evo_counter$countervalue-1))] # The last row of the most recent countervalue
 
-            save_data <- as_tibble(data$evo_extracted$'selected'[start_row:current_row])
-            range <- paste0("R", start_row+1, ":R", current_row+1)
+            evo_save_data <- as_tibble(data$evo_extracted$'selected'[start_row:current_row])
+            evo_range <- paste0("R", start_row+1, ":R", current_row+1)
 
             range_write(ss = evo_ID,
-                        data = save_data,
-                        range = range,
+                        data = evo_save_data,
+                        range = evo_range,
                         col_names = FALSE)
 
             evo_counter_upload$countervalue = 10
         }
     })
+    
+    observeEvent(social_counter$countervalue %% 10 == 0, {
+        if(social_counter$countervalue > 5 & social_counter$countervalue %% 10 == 0){ # Otherwise it has an issue with the countervalue
+
+            start_row <- which(data$social_extracted$'ID' == social_counter$countervalue - 10)[1] # The first row of the countervalue 5 people ago
+            current_row <- which(data$social_extracted$'ID' == social_counter$countervalue-1)[length(which(data$social_extracted$'ID' == social_counter$countervalue-1))] # The last row of the most recent countervalue
+
+            social_save_data <- as_tibble(data$social_extracted$'selected'[start_row:current_row])
+            social_range <- paste0("Q", start_row+1, ":Q", current_row+1)
+
+            range_write(ss = social_ID,
+                        data = social_save_data,
+                        range = social_range,
+                        col_names = FALSE)
+
+            social_counter_upload$countervalue = 10
+        }
+    })
+
+    observeEvent(bio_counter$countervalue %% 10 == 0, {
+        if(bio_counter$countervalue > 5 & bio_counter$countervalue %% 10 == 0){ # Otherwise it has an issue with the countervalue
+
+            start_row <- which(data$bio_extracted$'ID' == bio_counter$countervalue - 10)[1] # The first row of the countervalue 5 people ago
+            current_row <- which(data$bio_extracted$'ID' == bio_counter$countervalue-1)[length(which(data$bio_extracted$'ID' == bio_counter$countervalue-1))] # The last row of the most recent countervalue
+
+            bio_save_data <- as_tibble(data$bio_extracted$'selected'[start_row:current_row])
+            bio_range <- paste0("Q", start_row+1, ":Q", current_row+1)
+
+            range_write(ss = bio_ID,
+                        data = bio_save_data,
+                        range = bio_range,
+                        col_names = FALSE)
+
+            bio_counter_upload$countervalue = 10
+        }
+    })
 
     # Download
-    output$downloadData <- downloadHandler(
+    output$evo_download <- downloadHandler(
         filename = "evo_extracted.csv",
         content = function(file) {
-            write.csv(data$evo_extracted, file, row.names = FALSE)
+            write_csv(data$evo_extracted, na = "", file)
         })
+    
+    output$social_download <- downloadHandler(
+        filename = "social_extracted.csv",
+        content = function(file) {
+            write_csv(data$social_extracted, na = "", file)
+        })
+    
+    output$bio_download <- downloadHandler(
+        filename = "bio_extracted.csv",
+        content = function(file) {
+            write_csv(data$social_extracted, na = "", file)
+        })
+    
 
 })
--- scripts/rate-twitter-profiles/ui.R
+++ scripts/rate-twitter-profiles/ui.R
@@ -6,31 +6,26 @@ library(shiny)
 
 info_text <- "<h3> General Information </h3>
 This app allows a user to manually rate the twitter bios of evolutionary psychology, biology, and social psychology academics/researchers. 
-Toggle through the menu at the top to access these tasks. Please rate the Evo dataset first, as the Bio and Social Psych datasets require
-a fully rated, completely accurate Evo dataset. This app comes with associated datasets, sorted and saved in folders. Please make sure that
-you do not change the file names, as this will cause issues when you try to restart a session. <br><br>
+Toggle through the menu at the top to access these tasks.<br> <br> 
 
 <b> Taking a break: </b> You are welcome to close the app (website) whenever you wish. It will start back up where you left off. <br><br>
 
-<b> Saving: </b> The data for each of the tasks will be automatically saved in a .csv file every 50 profiles. It will also be saved when you 
-close the program. You are also welcome to save the data at any time.<br><br>
+<b> Loading: </b> Because of the size of the data it takes a while to load. Please be patient. <br><br>
 
-<b> About the app: </b> The purpose of this app is to create a dataset of evolutionary psychologists, biologists, and social psychologists, 
-that are as closely matched as possible based on number of twitter friends, number of twitter followers, gender, and country specified on Twitter.<br>
-    &emsp; First, the user (you) will select whether a Twitter user appears to be an evolutionary psychologist, based on their Twitter bio. Once complete, a
-dataset of users likely to be biology and social psychology researchers are matched with dataset of evolutionary psychologists. The user is then 
-asked to go through the biology and social psychology datasets to determine whether the users appear to be biologists or social psychologists, respectively.<br>
-    &emsp;Matching of biologists and social psychologists with evolutionary psychologists occurs using the pairmatch function from the optmatch library,
-which 'finds a pairing of treatment units to controls that minimizes the sum of discrepancies.' Because the biology and social psychology datasets 
-are very large and there are many cases where there appears to be more than one correct user, only cases with only 1 correct user were included. 
-These were matched to the evolutionary psychology dataset, and will be manually checked."
+<b> Saving: </b> The data for each of the tasks will be automatically uploaded to google sheets every 10 profiles. If you would like to exit the program,
+or you want to upload the data at any point yourself, you can do so using 'Manual Upload'.<br><br>
 
-evo_text_instructions <- " <h3> Instructions - Evo Psych Dataset </h3>
+<b> About the app: </b> The purpose of this app is to create a dataset of evolutionary psychologists, biologists, and social psychologists. In cases where 
+there is only one option, you are asked to decide whether this person is [relevant academic e.g., evo psych]. If there are multiple options, you are asked 
+to decide which - if any - is [relevant academic]. If there are no correct options, do not click on any names and instead press 'Next'."
+
+evo_text_instructions <- " <h3> Instructions - Evolutionary Psychology Dataset </h3>
 In this task you are asked to make a decision about which of a list of twitter users is most likely to be an evolutionary psychologist (e.g., researcher, 
-academic).based on their twitter bio and whether their twitter name matches the HBES list name. There may be cases where there is only one user. In that case,
+academic), based on their twitter bio and whether their twitter name matches the Human Behaviour and Evolution Society (HBES) list name. There may be cases 
+where there is only one user. In that case,
 simply decide whether that person is an evolutionary psychologist. If none of the twitter users appear to be an evolutionary psychologists, or if you are 
-unsure, simply press 'Next'. If you accidentally select a user, click on their name again to deselect. Your current selection will be displayed under the 
-table. You can use the 'Back' and 'Next' buttons to toggle through the users. <br> <br>
+unsure, simply press 'Next'. If you accidentally select a user, click on their name again to deselect. You can use the 'Back' and 'Next' buttons to toggle 
+through the users. <br> <br>
 
 <ul> Based on their twitter bio: 
 <li> <b> If 1 option: </b> Is this an evolutionary psychologist? 
@@ -46,32 +41,46 @@ Sometimes, more than one person matched both these criteria (came up in the sear
 match the HBES list were still extracted, due to the way Twitter searches. Last, some extracted users are not academics, even though they matched one of 
 the keywords. Hence, this task is to make sure that the final dataset consists only of evolutionary psychologists. <br> <br>
 
-Concerned about issues with the data entry? <a href='https://docs.google.com/spreadsheets/d/1rqghfdHF3ybOcnwI75O3Dsu-i_j8fIG0jCHL48zLJbc/edit?usp=sharing'>
+Concerned about issues with the data entry? <a href='https://docs.google.com/spreadsheets/d/15c_7wz0Mt62nLHFGtyWfkAX4BexDwVncYtUIyCYgnZQ/edit?usp=sharing'>
 See the raw dataset here.</a> <br><br> "
 
-bio_text <- "<h3> Instructions - Biology Dataset </h3>
-In this task you are asked to make a decision about whether a twitter bio belongs to a biologist (e.g., researcher, academic). If you believe the twitter
-bio belongs to a biologist, press 'Yes' button. If you believe it is not the twitter bio of a biologist, press the 'No' button. You will automatically be
-presented with a new bio each time you make a decision, but if you make a wrong decision, you can use the 'Back' and 'Next' buttons to toggle through the 
-users.<br><br>
+bio_text_instructions <- "<h3> Instructions - Evolutionary Biology Dataset </h3>
+In this task you are asked to make a decision about which of a list of twitter users is most likely to be an evolutionary biologist (e.g., researcher, 
+academic), based on their twitter bio and whether their twitter name matches the name present in the member directory of the Evolution Directory (EvolDir). 
+There may be cases where there is only one user. In that case,
+simply decide whether that person is an evolutionary biologist. If none of the twitter users appear to be an evolutionary biologists, or if you are 
+unsure, simply press 'Next'. If you accidentally select a user, click on their name again to deselect. You can use the 'Back' and 'Next' buttons to toggle 
+through the users. <br> <br>
 
-The program automatically matches Twitter users likely to be biologists with evolutionary psychologists. The user (you) then confirms whether they are 
-actually biologists based on the description on their Twitter bio. If they do not appear to be biologists, these people are removed from the dataset, and 
-matching occurs again with the new dataset. Eventually, after multiple rounds of manual evaluations, a full list of biologists should be matched to a list
-of evolutionary psychologists. At this point, it will say 'You're done!'"
+<ul> Based on their twitter bio: 
+<li> <b> If 1 option: </b> Is this an evolutionary biologist? 
+<li> <b> If 2+ options: </b> Which person, if any, is an evolutionary biologist?
+</ul>
 
-social_text <- "<h3> Instructions - Social Psychology Dataset </h3>
-In this task you are asked to make a decision about whether a twitter bio belongs to a social psychologist (e.g., researcher, academic). This task is 
-exactly the same as the biologist task, but with social psychologists. If you believe the twitter bio belongs to a social psychologist, press 'Yes' button.
-If you believe it is not the twitter bio of a social psychologist, press the 'No' button. You will automatically be presented with a new bio each time you 
-make a decision, but if you make a wrong decision, you can use the 'Back' and 'Next' buttons to toggle through the users.<br><br>
+<br> <br>"
 
-The program automatically matches Twitter users likely to be social psychologists with evolutionary psychologists. The user (you) then confirms whether they
-are actually social psychologists based on the description on their Twitter bio. If they do not appear to be social psychologists, these people are removed 
-from the dataset, and matching occurs again with the new dataset. Eventually, after multiple rounds of manual evaluations, a full list of social psychologists
-should be matched to a list of evolutionary psychologists. At this point, it will say 'You're done!'"
 
-# Define UI for application that draws a histogram
+bio_text_info <- "<br> <br>Concerned about issues with the data entry? <a href='https://docs.google.com/spreadsheets/d/17qnzHRX4RnJQfVCF1XmL0JZVruaFYROHph8GZjxIALU/edit?usp=sharing'>
+See the raw dataset here.</a> <br><br> "
+
+social_text_instructions <- "<h3> Instructions - Social Psychology Dataset </h3>
+In this task you are asked to make a decision about which of a list of twitter users is most likely to be a social psychologist (e.g., researcher, 
+academic), based on their twitter bio and whether their twitter name matches the name present in the member directory for the Society for Personality and
+Social Psychology (SPSP). There may be cases where there is only one user. In that case,
+simply decide whether that person is a social psychologist. If none of the twitter users appear to be a social psychologists, or if you are 
+unsure, simply press 'Next'. If you accidentally select a user, click on their name again to deselect. You can use the 'Back' and 'Next' buttons to toggle 
+through the users. <br> <br>
+
+<ul> Based on their twitter bio: 
+<li> <b> If 1 option: </b> Is this a social psychologist? 
+<li> <b> If 2+ options: </b> Which person, if any, is a social psychologist?
+</ul>
+
+<br> <br>"
+
+social_text_info <- "<br> <br>Concerned about issues with the data entry? <a href='https://docs.google.com/spreadsheets/d/1VrZfZEhRAK-_sHVmtvKNTIieE47Fkgpk9FrDL68sAE0/edit?usp=sharing'>
+See the raw dataset here.</a> <br><br> " 
+
 shinyUI(fluidPage(
     
     # App title
@@ -83,22 +92,43 @@ shinyUI(fluidPage(
                 tabPanel("Evo dataset",
                          HTML(evo_text_instructions),
                          column(12, align = "center",
-                                actionButton("decrease", "Back"), 
-                                actionButton("increase", "Next")
+                                actionButton("evo_decrease", "Back"), 
+                                actionButton("evo_increase", "Next")
                                 ),
                          DT::dataTableOutput("evo_table"),
-                         textOutput("count"),
-                         HTML(evo_text_info),
-                         actionButton("upload", "Manual Upload"),
-                         downloadButton("downloadData", "Download")),
+                         textOutput("evo_count"),
+                         textOutput("evo_total"),
+                         HTML("<br>"),
+                         actionButton("evo_upload", "Manual Upload"),
+                         downloadButton("evo_download", "Download"),
+                         HTML(evo_text_info)),
                 
-                
-                
+                tabPanel("Social dataset",
+                         HTML(social_text_instructions),
+                         column(12, align = "center",
+                                actionButton("social_decrease", "Back"), 
+                                actionButton("social_increase", "Next")
+                         ),
+                         DT::dataTableOutput("social_table"),
+                         textOutput("social_count"),
+                         textOutput("social_total"),
+                         HTML("<br>"),
+                         actionButton("social_upload", "Manual Upload"),
+                         downloadButton("social_download", "Download"),
+                         HTML(social_text_info)),
+    
                 tabPanel("Bio dataset",
-                         HTML(bio_text)),
-                
-                
-                tabPanel("Social psych dataset",
-                         HTML(social_text)))
+                         HTML(bio_text_instructions),
+                         column(12, align = "center",
+                                actionButton("bio_decrease", "Back"), 
+                                actionButton("bio_increase", "Next")
+                         ),
+                         DT::dataTableOutput("bio_table"),
+                         textOutput("bio_count"),
+                         textOutput("bio_total"),
+                         HTML("<br>"),
+                actionButton("bio_upload", "Manual Upload"),
+                downloadButton("bio_download", "Download"),
+                HTML(bio_text_info))),
     )
 )
